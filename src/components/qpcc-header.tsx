87c5bcd1ff0f3b8ecca65d6a681d97051774964e
mrkp <mark@zed.io>
2025-08-02 12:02:43 -0400

    fixing header routing

--- src/components/qpcc-header.tsx
+++ src/components/qpcc-header.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link"
+
 export function QPCCHeader() {
   return (
-    <div className="flex items-center">
+    <Link href="/" className="flex items-center">
       <img 
         src="/8-a-side-logo.png" 
         alt="QPCC 8-A-SIDE Football Tournament"
-        className="h-14 w-auto object-contain"
+        className="h-14 w-auto object-contain cursor-pointer hover:opacity-90 transition-opacity"
       />
-    </div>
+    </Link>
   )
 }
